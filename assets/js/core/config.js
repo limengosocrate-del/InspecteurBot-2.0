c856d71b2a4b7a6bb1d3e5e4208e70eb214e1cab
limengosocrate-del <limengosocrate@gmail.com>
2026-06-28 17:41:47 +0100

    Create config.js

---
+++ assets/js/core/config.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const VectorConfig = {
+    version: "5.0",
+    maxResults: 20,
+    cacheEnabled: true,
+    semanticBoost: 0.35,
+    vectorWeight: 0.45,
+    textWeight: 0.20,
+    rankingBoost: 0.15
+};
+
+module.exports = VectorConfig;
