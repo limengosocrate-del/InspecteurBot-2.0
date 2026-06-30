0d24e4a2f223b319353d4fd6c59a8585f9c4c589
limengosocrate-del <limengosocrate@gmail.com>
2026-06-30 08:39:21 +0100

    Update huggingface.js

--- assets/js/huggingface.js
+++ assets/js/huggingface.js
@@ -3,52 +3,42 @@
  HUGGING FACE API
 =========================================*/
 
-const HF_API_KEY =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;;
+const HF_API_KEY = "";
 
 async function demanderIA(question) {
 
     try {
 
         const response = await fetch(
-            "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.2",
+            "https://inspecteurb-ia.limengosocrate.workers.dev",
             {
                 method: "POST",
                 headers: {
-                    Authorization: "Bearer " + HF_API_KEY,
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify({
-                    inputs:
-                    "Tu es InspecteurBot RDC. Tu es un spécialiste du Code du Travail de la République Démocratique du Congo. Réponds uniquement en français.\n\n" + question
+                    question: question
                 })
             }
         );
 
         const data = await response.json();
 
+        if (Array.isArray(data) && data.length > 0) {
+            return data[0].generated_text;
+        }
+
         if (data.error) {
-            return "❌ " + data.error;
+            return "Erreur : " + data.error;
         }
 
-        if (Array.isArray(data)) {
-
-            if (data[0].generated_text) {
-                return data[0].generated_text;
-            }
-
-            if (data[0].summary_text) {
-                return data[0].summary_text;
-            }
-        }
-
-        return JSON.stringify(data);
+        return "Aucune réponse reçue.";
 
     } catch (e) {
 
         console.error(e);
-
-        return "Impossible de contacter Hugging Face.";
+        return "Impossible de contacter l'IA.";
 
     }
 
-             }
+         }
