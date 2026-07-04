80bbfe2a7626270c78cabe2b5b76e49dcaef350b
limengosocrate-del <limengosocrate@gmail.com>
2026-07-04 21:54:33 +0100

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,8 +1,13 @@
+// =======================================
+// INSPECTEURBOT IA RDC
+// Connexion Supabase
+// =======================================
+
 const SUPABASE_URL = "https://pohyykxhsdcctsamxqbl.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_2IY1P0xITKdQmZ03qt4arg_Tp8Ps-vW";
+const SUPABASE_ANON_KEY = "sb_publishable_2IY1P0xITKdQmZ03qt4arg_Tp8Ps-vW";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
-    SUPABASE_KEY
+    SUPABASE_ANON_KEY
 );
