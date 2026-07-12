b1364efde70dfd807fd054e78e7d4d1684d40ac5
limengosocrate-del <limengosocrate@gmail.com>
2026-07-12 16:10:46 +0100

    Update pv.routes.js

--- backend/routes/pv.routes.js
+++ backend/routes/pv.routes.js
@@ -1,1 +1,15 @@
+/**
+ * Routes API des PV.
+ * @module routes/pv.routes
+ */
+const router = require('express').Router();
+const ctrl = require('../controllers/pv.controller');
 
+router.post('/', ctrl.creer);
+router.get('/', ctrl.lister);
+router.get('/:id', ctrl.obtenir);
+router.put('/:id', ctrl.mettreAJour);
+router.delete('/:id', ctrl.supprimer);
+router.get('/:id/versions', ctrl.versions);
+
+module.exports = router;
